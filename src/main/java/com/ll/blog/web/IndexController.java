44642d842868e37833bc10f7b2ef043931a3cd58
李磊 <751475472@qq.com>
2022-08-05 18:39:03 +0800

    init

--- src/main/java/com/ll/blog/web/IndexController.java
+++ src/main/java/com/ll/blog/web/IndexController.java
@@ -2,13 +2,14 @@ package com.ll.blog.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class IndexController {
 
-    @GetMapping("/")
-    public String index(){
-        int i = 9/0;
+    @GetMapping("/{id}")
+    public String index(@PathVariable("id") Integer id){
+        //int i = 9/0;
         return "index";
     }
 }
